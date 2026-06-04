867837ae6214934aac71e0e6456791e626684ada
Fábio Barros <brainsonfire85@gmail.com>
2026-06-04 11:56:50 -0100

    Update API endpoints to use environment variable for base URL

--- services/apollo.ts
+++ services/apollo.ts
@@ -1,7 +1,6 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
-const graphqlUri =
-  process.env.NEXT_PUBLIC_DATA_API_URL ?? "http://localhost:8080/graphql";
+const graphqlUri = `${process.env.NEXT_PUBLIC_API_URL}/graphql`;
 
 const client = new ApolloClient({
   link: new HttpLink({
